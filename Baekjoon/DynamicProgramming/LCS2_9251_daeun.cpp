6abf699fa773a078dcc820ea887938f60a30adac
dharoo <haroo104@ajou.ac.kr>
2019-02-28 14:34:03 +0900

    for문

--- Baekjoon/DynamicProgramming/LCS2_9251_daeun.cpp
+++ Baekjoon/DynamicProgramming/LCS2_9251_daeun.cpp
@@ -1,10 +1,17 @@
-
+/*
+LCS 9251
+LCS(Longest Common Subsequence, 최장 공통 부분 수열)문제는 두 수열이 주어졌을 때, 
+모두의 부분 수열이 되는 수열 중 가장 긴 것을 찾는 문제
+시간제한 1초
+메모리제한 256MB
+*/
 #define MAX_STR_LEN 1000
 
 #include <cstdio>
 #include <algorithm>
 #include <cstring>
 
+// main내에서 선언하면 터진다. 스택영역, 데이터영역 크기가 다르기때문?
 int lcs[MAX_STR_LEN][MAX_STR_LEN];
 
 int main()
