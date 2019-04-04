e59fad4bd0f1885519542e42441851419a2f4028
dharoo <haroo104@ajou.ac.kr>
2019-04-04 16:13:10 +0900

    괄호

---
+++ Baekjoon/Stack/Bracket_1918.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AlgorithmTest2
+{
+    class Program
+    {
+        static bool IsCorrect(string str)
+        {
+            int count = 0;
+
+            foreach (char ch in str)
+            {
+                if (ch == '(')
+                    count++;
+                else
+                {
+                    if (count <= 0) return false;
+
+                    count--;
+                }
+            }
+
+            return count == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+            var n = int.Parse(line);
+
+            while ((n--) != 0)
+            {
+                Console.WriteLine(IsCorrect(Console.ReadLine()) ? "YES" : "NO");
+
+            }
+        }
+    }
+}
